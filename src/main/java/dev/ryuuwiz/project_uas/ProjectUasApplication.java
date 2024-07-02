dc54e779bbd83cb522db08d87a9851e770eda0dc
Ryu K <ryuuwiz@gmail.com>
2024-07-02 13:22:36 +0700

    chore: initial setup

---
+++ src/main/java/dev/ryuuwiz/project_uas/ProjectUasApplication.java
@@ -0,0 +1,13 @@
+package dev.ryuuwiz.project_uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUasApplication.class, args);
+	}
+
+}
